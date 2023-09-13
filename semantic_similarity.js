0e83983b862c90465a6158c41e357dc44482eb6d
Miriam Wisky <miriamwisky11@gmail.com>
2023-09-13 15:56:01 +0300

    semantic func

--- semantic_similarity.js
+++ semantic_similarity.js
@@ -1,11 +1,28 @@
+// const { exec } = require('child_process');
+
+// function calculateSemanticSimilarity(word1, word2) {
+//   return new Promise((resolve, reject) => {
+//     const command = `python -c "import semantic_func; print(semantic_func.semantic_similarity('${word1}', '${word2}'))"`;
+
+//     exec(command, (error, stdout, stderr) => {
+//      if (error) {
+//         reject(error);
+//       } else {
+//         const similarity = parseFloat(stdout);
+//         resolve(similarity);
+//       }
+//     });
+//   });
+// }
+// module.exports = { calculateSemanticSimilarity };
 const { exec } = require('child_process');
 
 function calculateSemanticSimilarity(word1, word2) {
   return new Promise((resolve, reject) => {
-    const command = `python -c "import semantic_func; print(semantic_func.semantic_similarity('${word1}', '${word2}'))"`;
+    const command = `python3 -c "import semantic_func; print(semantic_func.semantic_similarity('${word1}', '${word2}'))"`;
 
     exec(command, (error, stdout, stderr) => {
-     if (error) {
+      if (error) {
         reject(error);
       } else {
         const similarity = parseFloat(stdout);
@@ -14,4 +31,5 @@ function calculateSemanticSimilarity(word1, word2) {
     });
   });
 }
+
 module.exports = { calculateSemanticSimilarity };
