68adc6bfdd6deb785dd54ce7b326cfebab2680a2
Miriam Wisky <miriamwisky11@gmail.com>
2023-09-14 14:07:23 +0300

    spacy5

--- semantic_similarity.js
+++ semantic_similarity.js
@@ -1,30 +1,11 @@
-// const { exec } = require('child_process');
-
-// function calculateSemanticSimilarity(word1, word2) {
-//   return new Promise((resolve, reject) => {
-//     const command = `python -c "import semantic_func; print(semantic_func.semantic_similarity('${word1}', '${word2}'))"`;
-
-//     exec(command, (error, stdout, stderr) => {
-//      if (error) {
-//         reject(error);
-//       } else {
-//         const similarity = parseFloat(stdout);
-//         resolve(similarity);
-//       }
-//     });
-//   });
-// }
-// module.exports = { calculateSemanticSimilarity };
 const { exec } = require('child_process');
 
 function calculateSemanticSimilarity(word1, word2) {
   return new Promise((resolve, reject) => {
-    const pythonPath = 'C:\Users\Miriam\AppData\Local\Programs\Python\Python311\python.exe';
-    
-    const command = `${pythonPath} -c "import semantic_func; print(semantic_func.semantic_similarity('${word1}', '${word2}'))"`;
+    const command = `python -c "import semantic_func; print(semantic_func.semantic_similarity('${word1}', '${word2}'))"`;
 
     exec(command, (error, stdout, stderr) => {
-      if (error) {
+     if (error) {
         reject(error);
       } else {
         const similarity = parseFloat(stdout);
@@ -33,6 +14,25 @@ function calculateSemanticSimilarity(word1, word2) {
     });
   });
 }
-
 module.exports = { calculateSemanticSimilarity };
+// const { exec } = require('child_process');
+
+// function calculateSemanticSimilarity(word1, word2) {
+//   return new Promise((resolve, reject) => {
+//     const pythonPath = 'C:\Users\Miriam\AppData\Local\Programs\Python\Python311\python.exe';
+    
+//     const command = `${pythonPath} -c "import semantic_func; print(semantic_func.semantic_similarity('${word1}', '${word2}'))"`;
+
+//     exec(command, (error, stdout, stderr) => {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         const similarity = parseFloat(stdout);
+//         resolve(similarity);
+//       }
+//     });
+//   });
+// }
+
+// module.exports = { calculateSemanticSimilarity };
 
